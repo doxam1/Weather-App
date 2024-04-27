f58ccfb3affff4f275af562ec30c2959df0276d4
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-04-27 09:09:57 +0300

    basic fetching for weather location input

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
 import "../styles/style.css";
+
+const locationSubmit = document.querySelector('.locationSubmit');
+
+locationSubmit.onclick = () => {
+    const location = document.querySelector('#locationInput').value;
+
+    getWeather(location);
+}
+
+function getWeather(location) {
+    
+    fetch(`https://api.weatherapi.com/v1/current.json?key=5214792350054eb78b5162558241604&q=${location}`, {
+        mode: "cors",
+    })
+    .then (response => response.json())
+    .then (response => console.log(response.current.temp_c));
+
+}

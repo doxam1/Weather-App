911623c4f49c681e1d4cdef9cd427d098d168620
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-05-02 22:33:27 +0300

    background change for entered city

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,18 +8,31 @@ locationSubmit.onclick = () => {
   getWeather(location);
 };
 
-async function getWeather(location) {
-  weatherDisplayDiv.textContent = 'Loading.... tik tok...';
-  try {const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=5214792350054eb78b5162558241604&q=${location}&days=3`,
+function changeBackgroundToCityName(location) {
+  fetch(
+    `https://api.unsplash.com/photos/random?query=${location}&client_id=gmGkKtz8z4Ng_8T6yYRPFq5em6AzksLdSW4ybw-897g`,
     {
       mode: "cors",
     }
-  );
-  const weather = await response.json();
+  )
+    .then((response) => response.json())
+    .then((response) => document.body.style.background = `center / cover url(${response.urls.regular})`);
+}
 
-  weatherDisplayDiv.style.display = "flex";
-  weatherDisplayDiv.innerHTML = `
+async function getWeather(location) {
+  weatherDisplayDiv.textContent = "Loading.... tik tok...";
+  changeBackgroundToCityName(location);
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=5214792350054eb78b5162558241604&q=${location}&days=3`,
+      {
+        mode: "cors",
+      }
+    );
+    const weather = await response.json();
+
+    weatherDisplayDiv.style.display = "flex";
+    weatherDisplayDiv.innerHTML = `
         <h2 class="cityHeader"> ${weather.location.name}, ${weather.location.country}</h2>
         <div class="threeDayWeatherDivs">
         <div class="todayDiv"></div>
@@ -27,34 +40,34 @@ async function getWeather(location) {
         <div class="dayAfterTomorrowDiv"></div>
         </div>`;
 
-  const todayWeatherDiv = document.querySelector(".todayDiv");
-  todayWeatherDiv.style.background = `center/40% url('http:${weather.current.condition.icon}')`;
-  todayWeatherDiv.innerHTML = `
+    const todayWeatherDiv = document.querySelector(".todayDiv");
+    todayWeatherDiv.style.background = `center/40% url('http:${weather.current.condition.icon}')`;
+    todayWeatherDiv.innerHTML = `
         <h3> Today Weather </h3>
         <div> <b>Temp:</b> ${weather.current.temp_c} <div>
         <div> <b>Condition:</b> ${weather.current.condition.text} </div>        
         `;
 
-  const tomorrowWeatherDiv = document.querySelector(".tomorrowDiv");
-  tomorrowWeatherDiv.style.background = `center/40% url('http:${weather.forecast.forecastday[1].day.condition.icon}')`;
-  4;
-  tomorrowWeatherDiv.innerHTML = `
-      <h3> ${weather  .forecast.forecastday[1].date} </h3>
+    const tomorrowWeatherDiv = document.querySelector(".tomorrowDiv");
+    tomorrowWeatherDiv.style.background = `center/40% url('http:${weather.forecast.forecastday[1].day.condition.icon}')`;
+    4;
+    tomorrowWeatherDiv.innerHTML = `
+      <h3> ${weather.forecast.forecastday[1].date} </h3>
       <div> <b>Average temp:</b> ${weather.forecast.forecastday[1].day.avgtemp_c} <div>
       <div> <b>Condition:</b> ${weather.forecast.forecastday[1].day.condition.text} </div> 
       `;
 
-  const dayAfterTomorrowWeatherDiv = document.querySelector(
-    ".dayAfterTomorrowDiv"
-  );
-  dayAfterTomorrowWeatherDiv.style.background = `center/40% url('http:${weather.forecast.forecastday[2].day.condition.icon}')`;
-  dayAfterTomorrowWeatherDiv.innerHTML = `
+    const dayAfterTomorrowWeatherDiv = document.querySelector(
+      ".dayAfterTomorrowDiv"
+    );
+    dayAfterTomorrowWeatherDiv.style.background = `center/40% url('http:${weather.forecast.forecastday[2].day.condition.icon}')`;
+    dayAfterTomorrowWeatherDiv.innerHTML = `
       <h3> ${weather.forecast.forecastday[2].date} </h3>
       <div> <b>Average temp:</b> ${weather.forecast.forecastday[2].day.avgtemp_c} <div>
       <div> <b>Condition:</b> ${weather.forecast.forecastday[2].day.condition.text} </div> 
       `;
-} catch (msg) {
+  } catch (msg) {
     console.log(msg);
     weatherDisplayDiv.innerHTML = `<p> Sorry, I couldn't find a city or a country named ${location}<br>${msg}</p> `;
-  };
+  }
 }
